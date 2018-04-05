231d247fb99348b93b0d124b835d7e17bc961938
mitchh300 <mitchh300@gmail.com>
2018-04-05 09:31:37 -0400

    Update SnapNearFeatures.py

--- SnapNearFeatures.py
+++ SnapNearFeatures.py
@@ -5,25 +5,30 @@
 """
 This tool snaps multiple target features to input features based on a user specified SQL criteria and search radius.
 Import this script into an ArcGIS Toolbox.
-Note: The tool creates a scratch geodatabases in the ArcGIS default location on the machine this is run.
+Note: The tool uses the ArcGIS default scratch geodatabase.
 """
-import arcpy, os
+import arcpy
+import os
+import datetime
+
+#date string
+today = datetime.datetime.now().strftime('%m%d%Y')
 
 #Set scratch workspace environments
-workspace=arcpy.env.scratchGDB
+workspace = arcpy.env.scratchGDB
 outputWorkspace = workspace
 arcpy.env.overwriteOutput = True
 
 #Scratch workspace spatial join feature
-spatialJoinFC = outputWorkspace + '/' + "SnapFeatures_SpatialJoin"
+spatialJoinFC = os.path.join(outputWorkspace, "SnapFeatures_SpatialJoin_{}".format(today))
 
 #Get parameters
 inputFeatures = arcpy.GetParameterAsText(0) #Data Type = Feature Class, Required
 targetFeatures = arcpy.GetParameterAsText(1) #Data Type = Feature Class, Required
-inputQry = arcpy.GetParameterAsText(2)#Data Type = SQL Expression, Optional, Obtained from: InputFeatures
-targetQry = arcpy.GetParameterAsText(3)#Data Type = SQL Expression, Optional, Obtained from: TargetFeatures
-searchRadius = arcpy.GetParameterAsText(4)#Data Type = Linear Unit, Required
-uniqueID = arcpy.GetParameterAsText(5)#Data Type = Field, Required, Obtained from: TargetFeatures
+inputQry = arcpy.GetParameterAsText(2) #Data Type = SQL Expression, Optional, Obtained from: InputFeatures
+targetQry = arcpy.GetParameterAsText(3) #Data Type = SQL Expression, Optional, Obtained from: TargetFeatures
+searchRadius = arcpy.GetParameterAsText(4) #Data Type = Linear Unit, Required
+uniqueID = arcpy.GetParameterAsText(5) #Data Type = Field, Required, Obtained from: TargetFeatures
 arcpy.AddFieldDelimiters(targetFeatures, uniqueID)
 
 #Apply SQL query to both features
@@ -33,21 +38,21 @@ arcpy.MakeFeatureLayer_management(targetFeatures, "targetLayer", targetQry)
 #Preform spatial join
 arcpy.SpatialJoin_analysis("inputLayer", "targetLayer", spatialJoinFC, "JOIN_ONE_TO_ONE", "KEEP_ALL", "", "CLOSEST", searchRadius, "")
 
-#Search through spatial join features and grab spatial geometry tokens 
-tokens = []
-with arcpy.da.SearchCursor(spatialJoinFC, ['%s_1' % uniqueID, 'SHAPE@X','SHAPE@Y'])  as cursor:
-    for row in cursor:
-        tokens.append(str(row[0]) + '|' + str(row[1]) + '|' + str(row[2]))
-arcpy.AddMessage("\nGrabbed geometry tokens from input features.")
+#build dict from spatial join feature geometry tokens
+uniqueID_1 = "{}_1".format(uniqueID)
+excludeQry = "{} IS NOT NULL".format(uniqueID_1)
+tCursor = arcpy.da.SearchCursor(spatialJoinFC, ["{}_1".format(uniqueID_1), "SHAPE@XY"], excludeQry)
+tokens = {row[0]:row[1] for row in tCursor}
 
 #Update geometry tokens based on token list
-count = 0
-with arcpy.da.UpdateCursor(targetFeatures, [uniqueID,'SHAPE@X','SHAPE@Y']) as cursor:
-    for row in cursor:
-        for x in tokens:
-            if x.split('|')[0] == str(row[0]):
-                row[1] = float(x.split('|')[1])
-                row[2] = float(x.split('|')[2])
-                count += 1
-                cursor.updateRow(row)
-arcpy.AddMessage("\n%s features were snapped to input features locations.\n" % count)
+updateCount = 0
+with arcpy.da.UpdateCursor(targetFeatures, [uniqueID,'SHAPE@XY']) as cursor:
+    for iterCount, row in enumerate(cursor, 1):
+        if row[0] in tokens:
+            row[1] = tokens[row[0]]
+            cursor.updateRow(row)
+            updateCount += 1
+           
+del tCursor
+del cursor
+arcpy.AddMessage("\n{0}s features were snapped to input feature locations.\n".format(count))
