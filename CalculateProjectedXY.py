90b455df30bb26180ab976d67b744965ad782d82
mitchh300 <mitchh300@gmail.com>
2018-09-18 15:16:59 -0400

    Update CalculateProjectedXY.py

--- CalculateProjectedXY.py
+++ CalculateProjectedXY.py
@@ -1,7 +1,9 @@
-""" Verision: 2.7.11
+""" 
+    Version: 2.7.11
     Description: Calculates X Y values for a point feature class in a specified projected coordinate system.
     Notes: This code is a verion of a code posted to StackExchange by user crmackey. 
-           See here: http://gis.stackexchange.com/questions/158923/calculating-geometry-for-feature-class-with-x-y-and-lat-long-values-in-different
+           See here: http://gis.stackexchange.com/questions/158923/calculating-geometry-for-feature-class-with \ 
+           -x-y-and-lat-long-values-in-different
 """
 
 
@@ -12,9 +14,15 @@ feature = r'...path to feature class...'
 #This spatial reference is EPSG code 2278 [NAD83 / Texas South Central (US_FOOT)]
 ref = arcpy.SpatialReference(2278)
 
-with arcpy.da.UpdateCursor(feature, ['SHAPE@','POINT_X','POINT_Y']) as cursor:
+#lat/long fields to write to 
+Xfield = 'X'
+Yfield = 'Y'
+
+#iterate over rows and update using .projectAs arcpy function
+with arcpy.da.UpdateCursor(feature, ['SHAPE@', Xfield, Yfield]) as cursor:
     for row in cursor:
         pnt_proj = row[0].projectAs(ref)
         row[1] = [pnt_proj.centroid.X]
         row[2]= [pnt_proj.centroid.Y]
         cursor.updateRow(row)
+del cursor
