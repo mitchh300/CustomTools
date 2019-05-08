5ede77010332302844fda3ee842b59feab973f81
mitchh300 <mitchh300@gmail.com>
2019-05-08 14:19:22 -0400

    Update ExportAttachments.py

--- ExportAttachments.py
+++ ExportAttachments.py
@@ -6,14 +6,15 @@ import arcpy
 from arcpy import da
 import os
 
-#Get input table from user
+#Get input ATTACH table from user
 inTable = arcpy.GetParameterAsText(0)
 
 #Get output folder location from user
 fileLocation = arcpy.GetParameterAsText(1)
 
 #Search through input table and extract attachments to specified folder
-with da.SearchCursor(inTable, ['DATA','REL_GLOBALID']) as cursor: 
+#may need to change 'REL_OBJECTID' to the related OID from the FC with attachments
+with da.SearchCursor(inTable, ['DATA','REL_OBJECTID']) as cursor: 
     for item in cursor:
         attachment = item[0]
         filename = str(item[1]) + '.jpg' #Edit name of output file by specifying in the SearchCursor, edit data type if needed
